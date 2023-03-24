06fbd753f3c2be80d0711c2d473b2f3b56d5b28b
zbroozy <willianlivialopes@hotmail.com>
2023-03-23 22:22:23 -0300

    Creating the body of the project.

---
+++ PlayerApplication.UnitTest/UnitTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PlayerApplication.UnitTest
+{
+    [TestClass]
+    public class UnitTest
+    {
+        [TestMethod]
+        public void TestMethod()
+        {
+        }
+    }
+}
